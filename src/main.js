9cb1c5b3b7383b4e0c4ed769664176e1b2ce1207
Jeremyrts <jeremyrts@Pumba.local>
2020-02-02 16:40:33 +0100

    Fixed external script import

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import LoadScript from 'vue-plugin-load-script';
+import LoadScript from "vue-plugin-load-script";
 
 Vue.use(LoadScript);
 
