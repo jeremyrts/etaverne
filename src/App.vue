1e311b7cda4b78dfaebcd54eb20cc8cd6ef42bd2
Jeremyrts <jeremyrts@Pumba.local>
2020-02-02 18:45:24 +0100

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <img class="image" src="./assets/logo_etaverne.png" alt="">
-      <router-link class="item-nav" to="/">Live</router-link>
-      <router-link class="item-nav" to="/programmation">Programmation</router-link>
-      <router-link class="item-nav" to="/videos">Vidéos</router-link>
-      <router-link class="item-nav" to="/a-propos">À propos</router-link>
+      <div class="up-part"> 
+        <div class="image-container">
+          <img class="image" src="./assets/logo_etaverne.png" alt="">
+        </div>
+        <div class="item-container">
+          <router-link class="item-nav" to="/">Live</router-link>
+          <router-link class="item-nav" to="/programmation">Programmation</router-link>
+          <router-link class="item-nav" to="/videos">Vidéos</router-link>
+          <router-link class="item-nav" to="/a-propos">À propos</router-link>
+        </div>
+      </div>
+      <div class="low-part">
+        <p>E-Taverne 2020</p>
+        <div class="social-media-footer">
+          <span>Les reseaux sociaux</span>
+        </div>
+      </div>
     </div>
     <router-view />
   </div>
@@ -21,14 +33,15 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #212121;
   display: flex;
   flex-direction: row;
-  border: solid salmon 2px;
+  /* border: solid salmon 2px; */
   height: 100%;
   background-image: url("./assets/background.png");
   background-repeat: repeat;
 }
+
 html,
 body {
   margin: 0;
@@ -41,19 +54,33 @@ body {
   padding: 30px;
   display: flex;
   flex-direction: column;
-  /* position:fixed; */
+  justify-content: space-between;
+  /* position: fixed; */
+  /* height: 100%; */
   /* border: solid black 2px; */
   background-color: #212121
 }
 
-#nav a {
+#nav a, span {
   font-weight: bold;
   /* color: #2c3e50; */
   color: white;
 }
+#nav a {
+  text-decoration: none;
+}
+
+#nav p {
+  color: white;
+}
+
 
 #nav a.router-link-exact-active {
   color: #992BE5
+}
+.item-container {
+  display: flex;
+  flex-direction: column
 }
 .item-nav {
   margin: 20px 0;
