aef3015394da55cceabc1c6fccc87852cda45276
Jeremyrts <jeremyrts@Pumba.local>
2020-02-12 10:00:40 +0100

    Fixed reload js when switching back from the menu

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -10,65 +10,70 @@
 </template>
 
 <script>
-export default {};
-let videos;
-let XML = new XMLHttpRequest();
-XML.open("GET", "https://api.twitch.tv/helix/videos?user_id=268985773");
-XML.setRequestHeader("Client-ID", "d75dgiycfc2gqlbl2l6z2g0pk9er4m");
-XML.send();
-XML.onload = function() {
-  videos = JSON.parse(XML.response);
-  console.log(videos);
-  let html = "";
-  console.log(html);
-  for (let videoIndex in videos.data) {
-    let videoItem = videos.data[videoIndex];
-    let background = videoItem.thumbnail_url;
-    background = background.replace("-%{width}x%{height}.jpg", "-320x180.jpg");
-    html +=
-      `<a target="_blank" href="` +
-      videoItem.url +
-      `" class="grid-item"><img class="item-image" src="` +
-      background +
-      `"/><div class="item-title">` +
-      videoItem.title +
-      `</div></a>`;
-    if (videoIndex >= 6) {
-      html += `<a target="_blank" href="https://www.twitch.tv/e_taverne/videos" class="grid-item"><img class="item-image" src="'./assets/plus.png'" alt=""/><div class="item-title">Voir plus</div></a>`;
-      break;
-    }
+export default {
+  name: "Videos",
+  mounted() {
+    let videos;
+    let XML = new XMLHttpRequest();
+    XML.open("GET", "https://api.twitch.tv/helix/videos?user_id=268985773");
+    XML.setRequestHeader("Client-ID", "d75dgiycfc2gqlbl2l6z2g0pk9er4m");
+    XML.send();
+    XML.onload = function() {
+      videos = JSON.parse(XML.response);
+      console.log(videos);
+      let html = "";
+      console.log(html);
+      for (let videoIndex in videos.data) {
+        let videoItem = videos.data[videoIndex];
+        let background = videoItem.thumbnail_url;
+        background = background.replace("-%{width}x%{height}.jpg", "-320x180.jpg");
+        html +=
+        `<a target="_blank" href="` +
+        videoItem.url +
+        `" class="grid-item"><img class="item-image" src="` +
+        background +
+        `"/><div class="item-title">` +
+        videoItem.title +
+        `</div></a>`;
+        if (videoIndex >= 6) {
+          html += `<a target="_blank" href="https://www.twitch.tv/e_taverne/videos" class="grid-item"><img class="item-image" src="'./assets/plus.png'" alt=""/><div class="item-title">Voir plus</div></a>`;
+        break;
+        }
+      }
+      document.getElementById("videos-twitch").innerHTML = html;
+    };
+    let clips;
+    let XMLClip = new XMLHttpRequest();
+    XMLClip.open(
+      "GET",
+      "https://api.twitch.tv/helix/clips?broadcaster_id=268985773"
+    );
+    XMLClip.setRequestHeader("Client-ID", "d75dgiycfc2gqlbl2l6z2g0pk9er4m");
+    XMLClip.send();
+    XMLClip.onload = function() {
+      clips = JSON.parse(XMLClip.response);
+      let htmlClip = "";
+      for (let clipIndex in clips.data) {
+        let clipItem = clips.data[clipIndex];
+        htmlClip +=
+        `<a target="_blank" href="` +
+        clipItem.url +
+        `" class="grid-item"><img class="item-image" src="` +
+        clipItem.thumbnail_url +
+        `"/><div class="item-title">` +
+        clipItem.title +
+        `</div></a>`;
+        if (clipIndex >= 6) {
+          htmlClip += `<a target="_blank" href="https://www.twitch.tv/e_taverne/clips" class="grid-item"><img class="item-image" src="'./assets/plus.png'" alt=""/><div class="item-title">Voir plus</div></a>`;
+          break;
+        }
+      }
+      console.log(htmlClip);
+      document.getElementById("clips-twitch").innerHTML = htmlClip;
+    };
   }
-  document.getElementById("videos-twitch").innerHTML = html;
 };
-let clips;
-let XMLClip = new XMLHttpRequest();
-XMLClip.open(
-  "GET",
-  "https://api.twitch.tv/helix/clips?broadcaster_id=268985773"
-);
-XMLClip.setRequestHeader("Client-ID", "d75dgiycfc2gqlbl2l6z2g0pk9er4m");
-XMLClip.send();
-XMLClip.onload = function() {
-  clips = JSON.parse(XMLClip.response);
-  let htmlClip = "";
-  for (let clipIndex in clips.data) {
-    let clipItem = clips.data[clipIndex];
-    htmlClip +=
-      `<a target="_blank" href="` +
-      clipItem.url +
-      `" class="grid-item"><img class="item-image" src="` +
-      clipItem.thumbnail_url +
-      `"/><div class="item-title">` +
-      clipItem.title +
-      `</div></a>`;
-    if (clipIndex >= 6) {
-      htmlClip += `<a target="_blank" href="https://www.twitch.tv/e_taverne/clips" class="grid-item"><img class="item-image" src="'./assets/plus.png'" alt=""/><div class="item-title">Voir plus</div></a>`;
-      break;
-    }
-  }
-  console.log(htmlClip);
-  document.getElementById("clips-twitch").innerHTML = htmlClip;
-};
+
 </script>
 <style>
 .grid-item {
